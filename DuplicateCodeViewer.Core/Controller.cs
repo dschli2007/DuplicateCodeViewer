b1de4f2ccf7a4985cd586b792299262caf9febff
Dirceu <dschli2007@gmail.com>
2018-01-20 20:46:54 -0200

    Add Controller class
    This class will receive a xml file path and read data from file

---
+++ DuplicateCodeViewer.Core/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using DuplicateCodeViewer.Core.Metadata;
+
+namespace DuplicateCodeViewer.Core
+{
+    public class Controller : IController
+    {
+        public void Load(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler DataUpdated;
+
+        public IEnumerable<Duplicate> Duplicates { get; }
+
+        public IEnumerable<SourceFile> UniqueFiles { get; }
+    }
+}
---
+++ DuplicateCodeViewer.Core/IController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using DuplicateCodeViewer.Core.Metadata;
+
+namespace DuplicateCodeViewer.Core
+{
+    public interface IController
+    {
+        void Load(string filename);
+
+        event EventHandler DataUpdated;
+
+        IEnumerable<Duplicate> Duplicates { get; }
+
+        IEnumerable<SourceFile> UniqueFiles { get; }
+    }
+}
