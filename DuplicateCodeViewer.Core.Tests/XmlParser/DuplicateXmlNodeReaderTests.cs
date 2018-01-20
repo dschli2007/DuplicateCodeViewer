042d054d82fad61d372b3a2de2c0048df31d2956
Dirceu <dschli2007@gmail.com>
2018-01-20 13:20:58 -0200

    Tests for XmlParser (cover 100%)

---
+++ DuplicateCodeViewer.Core.Tests/XmlParser/DuplicateXmlNodeReaderTests.cs
@@ -0,0 +1,109 @@
+﻿using System.Xml;
+using DuplicateCodeViewer.Core.XmlParser;
+using NUnit.Framework;
+
+namespace DuplicateCodeViewer.Core.Tests.XmlParser
+{
+    [TestFixture]
+    public class DuplicateXmlNodeReaderTests
+    {
+
+        private static string DefaultXml = @"<Duplicate Cost=""210"">
+      <Fragment>
+        <FileName>..\file1.cs</FileName>
+        <OffsetRange Start=""3027"" End=""3564""></OffsetRange>
+        <LineRange Start=""72"" End=""82""></LineRange>
+      </Fragment>
+      <Fragment>
+        <FileName>..\file2.cs</FileName>
+        <OffsetRange Start=""3911"" End=""4448""></OffsetRange>
+        <LineRange Start=""92"" End=""102""></LineRange>
+      </Fragment>
+    </Duplicate>";
+
+        private XmlDocument _document;
+        private XmlNode _duplicateNode;
+
+
+        private DuplicateXmlNodeReader CreateReader()
+        {
+            return new DuplicateXmlNodeReader();
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            _document = new XmlDocument();
+            _document.LoadXml(DefaultXml);
+            _duplicateNode = _document.GetElementsByTagName("Duplicate")[0];
+        }
+
+        [Test]
+        public void Read_WhenHasCostValue_ShouldReadCost()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+
+            Assert.AreEqual(210, reader.Cost);
+        }
+
+        [Test]
+        public void Read_WhenHasNoCostValue_ShouldReadDefaultCost()
+        {
+            _duplicateNode?.Attributes?.RemoveAll();
+
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+
+            Assert.AreEqual(1, reader.Cost);
+        }
+
+        [Test]
+        public void Read_WhenValidNode_ShouldReadFragmentFileName1()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+            Assert.AreEqual(@"..\file1.cs", reader.Fragment1.Filename);
+        }
+
+        [Test]
+        public void Read_WhenValidNode_ShouldReadFragmentLineStart1()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+            Assert.AreEqual(72, reader.Fragment1.LineStart);
+        }
+
+        [Test]
+        public void Read_WhenValidNode_ShouldReadFragmentLineEnd1()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+            Assert.AreEqual(82, reader.Fragment1.LineEnd);
+        }
+        
+        [Test]
+        public void Read_WhenValidNode_ShouldReadFragmentFileName2()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+            Assert.AreEqual(@"..\file2.cs", reader.Fragment2.Filename);
+        }
+        
+        [Test]
+        public void Read_WhenValidNode_ShouldReadFragmentLineStart2()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+            Assert.AreEqual(92, reader.Fragment2.LineStart);
+        }
+
+        [Test]
+        public void Read_WhenValidNode_ShouldReadFragmentLineEnd2()
+        {
+            var reader = CreateReader();
+            reader.Read(_duplicateNode);
+            Assert.AreEqual(102, reader.Fragment2.LineEnd);
+        }
+    }
+}
--- DuplicateCodeViewer.Core.Tests/XmlParser/XmlParserObservableTests.cs
+++ DuplicateCodeViewer.Core.Tests/XmlParser/XmlParserObservableTests.cs
@@ -1,4 +1,5 @@
-﻿using DuplicateCodeViewer.Core.Metadata;
+﻿using System.Xml;
+using DuplicateCodeViewer.Core.Metadata;
 using DuplicateCodeViewer.Core.Tests.Fakes;
 using DuplicateCodeViewer.Core.XmlParser;
 using NUnit.Framework;
@@ -13,6 +14,37 @@ namespace DuplicateCodeViewer.Core.Tests.XmlParser
             var sourceFileBuilder = new SourceFileBuilderFlyWeightFake();
             return new XmlParserObservable(sourceFileBuilder);
         }
+        
+        private XmlDocument GetXmlDocument()
+        {
+            const string sampleDocument = @"<Samples><Duplicate Cost=""210"">
+      <Fragment>
+        <FileName>..\file1.cs</FileName>
+        <OffsetRange Start=""3027"" End=""3564""></OffsetRange>
+        <LineRange Start=""72"" End=""82""></LineRange>
+      </Fragment>
+      <Fragment>
+        <FileName>..\file2.cs</FileName>
+        <OffsetRange Start=""3911"" End=""4448""></OffsetRange>
+        <LineRange Start=""92"" End=""102""></LineRange>
+      </Fragment>
+    </Duplicate><Duplicate Cost=""211"">
+      <Fragment>
+        <FileName>..\file3.cs</FileName>
+        <OffsetRange Start=""3027"" End=""3564""></OffsetRange>
+        <LineRange Start=""73"" End=""83""></LineRange>
+      </Fragment>
+      <Fragment>
+        <FileName>..\file4.cs</FileName>
+        <OffsetRange Start=""3911"" End=""4448""></OffsetRange>
+        <LineRange Start=""93"" End=""103""></LineRange>
+      </Fragment>
+    </Duplicate></Samples>";
+            var result = new XmlDocument();
+            result.LoadXml(sampleDocument);
+            return result;
+        }
+
 
         [Test]
         public void AddObserver_WhenValidObserver_ShouldAddObserverToObserverList()
@@ -40,7 +72,7 @@ namespace DuplicateCodeViewer.Core.Tests.XmlParser
 
             parser.RemoveObserver(sampleObserver);
             parser.Notify(sampleDuplicate);
-            
+
             Assert.AreEqual(1, sampleObserver.Duplicates.Count);
         }
 
@@ -55,9 +87,72 @@ namespace DuplicateCodeViewer.Core.Tests.XmlParser
             parser.AddObserver(sampleObserver1);
             parser.AddObserver(sampleObserver2);
             parser.Notify(sampleDuplicate);
-            
+
             Assert.AreEqual(1, sampleObserver1.Duplicates.Count);
             Assert.AreEqual(1, sampleObserver2.Duplicates.Count);
+        }
+
+        [Test]
+        public void Parse_WhenValidDocument_ShouldParseFilenames()
+        {
+            var observer = new XmlParserObserverFake();
+
+            var parser = CreateParser();
+            parser.AddObserver(observer);
+            parser.Parse(GetXmlDocument());
+
+            Assert.AreEqual(2, observer.Duplicates.Count);
+            Assert.AreEqual(@"..\file1.cs", observer.Duplicates[0].Fragment1.SourceFile.Filename);
+            Assert.AreEqual(@"..\file2.cs", observer.Duplicates[0].Fragment2.SourceFile.Filename);
+            Assert.AreEqual(@"..\file3.cs", observer.Duplicates[1].Fragment1.SourceFile.Filename);
+            Assert.AreEqual(@"..\file4.cs", observer.Duplicates[1].Fragment2.SourceFile.Filename);
+        }
+
+        [Test]
+        public void Parse_WhenValidDocument_ShouldParseCosts()
+        {
+            var observer = new XmlParserObserverFake();
+
+            var parser = CreateParser();
+            parser.AddObserver(observer);
+            parser.Parse(GetXmlDocument());
+
+            Assert.AreEqual(2, observer.Duplicates.Count);
+            Assert.AreEqual(210, observer.Duplicates[0].Cost);
+            Assert.AreEqual(211, observer.Duplicates[1].Cost);
+        }
+
+        [Test]
+        public void Parse_WhenValidDocument_ShouldParseLineStart()
+        {
+            var observer = new XmlParserObserverFake();
+
+            var parser = CreateParser();
+            parser.AddObserver(observer);
+            parser.Parse(GetXmlDocument());
+
+            Assert.AreEqual(2, observer.Duplicates.Count);
+            Assert.AreEqual(72, observer.Duplicates[0].Fragment1.LineStart);
+            Assert.AreEqual(92, observer.Duplicates[0].Fragment2.LineStart);
+            Assert.AreEqual(73, observer.Duplicates[1].Fragment1.LineStart);
+            Assert.AreEqual(93, observer.Duplicates[1].Fragment2.LineStart);
+        }
+
+        [Test]
+        public void Parse_WhenValidDocument_ShouldParseLineEnd()
+        {
+            var observer = new XmlParserObserverFake();
+
+            var parser = CreateParser();
+            parser.AddObserver(observer);
+            parser.Parse(GetXmlDocument());
+
+            Assert.AreEqual(2, observer.Duplicates.Count);
+            Assert.AreEqual(82, observer.Duplicates[0].Fragment1.LineEnd);
+            Assert.AreEqual(102, observer.Duplicates[0].Fragment2.LineEnd);
+            Assert.AreEqual(83, observer.Duplicates[1].Fragment1.LineEnd);
+            Assert.AreEqual(103, observer.Duplicates[1].Fragment2.LineEnd);
+
         }
 
     }
