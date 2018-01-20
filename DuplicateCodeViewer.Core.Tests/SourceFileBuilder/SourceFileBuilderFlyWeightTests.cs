8f00474dd377b300da0ff92296ac8314ffeec596
Dirceu <dschli2007@gmail.com>
2018-01-20 13:36:53 -0200

    Implement of SourceFileBuilderFlyWeight and tests

---
+++ DuplicateCodeViewer.Core.Tests/SourceFileBuilder/SourceFileBuilderFlyWeightTests.cs
@@ -0,0 +1,34 @@
+﻿using DuplicateCodeViewer.Core.SourceFileBuilder;
+using NUnit.Framework;
+
+namespace DuplicateCodeViewer.Core.Tests.SourceFileBuilder
+{
+    [TestFixture]
+    public class SourceFileBuilderFlyWeightTests
+    {
+
+        [TestCase("file1.txt")]
+        [TestCase("file2.txt")]
+        public void GetSourceFile_WhenSameFileName_ShoudReturnSameObject(string filename)
+        {
+            var builder = new SourceFileBuilderFlyWeight();
+            var result1 = builder.GetSourceFile(filename);
+            var result2 = builder.GetSourceFile(filename);
+            Assert.AreSame(result1, result2);
+        }
+
+        [Test]
+        public void GetSourceFile_WhenMultipleFiles_ShoudReturnSameObjectForAll()
+        {
+            var file1 = "a.txt";
+            var file2 = "b.txt";
+            var builder = new SourceFileBuilderFlyWeight();
+            var result1 = builder.GetSourceFile(file1);
+            var result2 = builder.GetSourceFile(file2);
+            var result1A = builder.GetSourceFile(file1);
+            var result2A = builder.GetSourceFile(file2);
+            Assert.AreSame(result1, result1A);
+            Assert.AreSame(result2, result2A);
+        }
+    }
+}
--- DuplicateCodeViewer.Core/SourceFileBuilder/SourceFileBuilderFlyWeight.cs
+++ DuplicateCodeViewer.Core/SourceFileBuilder/SourceFileBuilderFlyWeight.cs
@@ -1,12 +1,24 @@
-﻿using DuplicateCodeViewer.Core.Metadata;
+﻿using System.Collections.Generic;
+using DuplicateCodeViewer.Core.Metadata;
 
 namespace DuplicateCodeViewer.Core.SourceFileBuilder
 {
     public class SourceFileBuilderFlyWeight : ISourceFileBuilderFlyWeight
     {
+        private readonly Dictionary<string, SourceFile> _files = new Dictionary<string, SourceFile>();   
+        private readonly object _filesLock = new object();
+        
         public SourceFile GetSourceFile(string filename)
         {
-            throw new System.NotImplementedException();
+            lock (_filesLock)
+            {
+                if (_files.ContainsKey(filename))
+                    return _files[filename];
+
+                var newFile = new SourceFile{ Filename = filename};
+                _files[filename] = newFile;
+                return newFile;
+            }
         }
     }
 }
