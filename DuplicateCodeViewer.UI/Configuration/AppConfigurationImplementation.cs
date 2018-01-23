18072830a0c36292e5140f882583f4f24bdae206
Dirceu <dschli2007@gmail.com>
2018-01-23 21:43:20 -0200

    Add Configuration ability

---
+++ DuplicateCodeViewer.UI/Configuration/AppConfigurationImplementation.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace DuplicateCodeViewer.UI.Configuration
+{
+    public class AppConfigurationImplementation : IAppConfiguration
+    {
+        
+        public string DupFinderExe
+        {
+            get => ConfigurationManager.AppSettings["dupFinderExe"];
+            set => ConfigurationManager.AppSettings["dupFinderExe"] = value;
+        }
+        
+    }
+}
---
+++ DuplicateCodeViewer.UI/Configuration/IAppConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace DuplicateCodeViewer.UI.Configuration
+{
+    public interface IAppConfiguration
+    {
+        string DupFinderExe { get; set; }
+    }
+}
