c6a966c07d914c2f7f034567b529c6598dbd1b5a
Dirceu <dschli2007@gmail.com>
2018-01-23 21:43:50 -0200

    Add select dupfinder.exe to configuration

--- DuplicateCodeViewer.UI/UserInterfaceCommands/SelectDupFinderExeCommand.cs
+++ DuplicateCodeViewer.UI/UserInterfaceCommands/SelectDupFinderExeCommand.cs
@@ -1,10 +1,19 @@
-﻿namespace DuplicateCodeViewer.UI.UserInterfaceCommands
+﻿using System.Windows.Forms;
+using DuplicateCodeViewer.UI.Configuration;
+
+namespace DuplicateCodeViewer.UI.UserInterfaceCommands
 {
     internal class SelectDupFinderExeCommand : IUserInterfaceCommand
     {
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            var openFile = new OpenFileDialog {Filter = @"DupFinder.exe|DupFinder.exe"};
+            var dialogResult = openFile.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var appConfig = new AppConfigurationImplementation();
+                appConfig.DupFinderExe = openFile.FileName;
+            }
         }
     }
 }
