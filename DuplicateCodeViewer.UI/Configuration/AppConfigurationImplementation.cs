8aead32773b8499bc685846a8861825cd533d5c6
Dirceu <dschli2007@gmail.com>
2018-01-23 22:07:50 -0200

    Adjust configuration

--- DuplicateCodeViewer.UI/Configuration/AppConfigurationImplementation.cs
+++ DuplicateCodeViewer.UI/Configuration/AppConfigurationImplementation.cs
@@ -4,12 +4,31 @@ namespace DuplicateCodeViewer.UI.Configuration
 {
     public class AppConfigurationImplementation : IAppConfiguration
     {
-        
+        private System.Configuration.Configuration _configFile;
+
+        public AppConfigurationImplementation()
+        {
+            _configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        }
+
+
         public string DupFinderExe
         {
-            get => ConfigurationManager.AppSettings["dupFinderExe"];
-            set => ConfigurationManager.AppSettings["dupFinderExe"] = value;
+            get
+            {
+                return _configFile.AppSettings.Settings["dupFinderExe"].Value;
+            }
+            set
+            {
+                var settings = _configFile.AppSettings.Settings;
+                settings.Remove(@"dupFinderExe");
+                settings.Add(@"dupFinderExe", value);
+                _configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(_configFile.AppSettings.SectionInformation.Name);
+            }
         }
-        
+
+
+
     }
 }
--- DuplicateCodeViewer.UI/UserInterfaceCommands/OpenVisualStudioFileCommand.cs
+++ DuplicateCodeViewer.UI/UserInterfaceCommands/OpenVisualStudioFileCommand.cs
@@ -1,10 +1,47 @@
-﻿namespace DuplicateCodeViewer.UI.UserInterfaceCommands
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using DuplicateCodeViewer.Core.LoadController;
+using DuplicateCodeViewer.UI.Configuration;
+
+namespace DuplicateCodeViewer.UI.UserInterfaceCommands
 {
-    internal class OpenVisualStudioFileCommand : IUserInterfaceCommand
+    internal class OpenVisualStudioFileCommand : IUserInterfaceCommand, IXmlFileSource
     {
         public void Execute()
         {
-            
+            string file;
+            if (TryToPickFile(out file))
+            {
+                Filename = CreateDuplicateReportFile(file);
+                CanLoad = true;
+            }
         }
+
+        private string CreateDuplicateReportFile(string file)
+        {
+            var temp = Path.Combine(Environment.CurrentDirectory, Path.GetFileNameWithoutExtension(file) + ".xml");
+
+            var config = new AppConfigurationImplementation();
+            var exe = config.DupFinderExe;
+            var parameters = $"-o:{temp} {file}";
+            var process = Process.Start(exe, parameters);
+            process?.WaitForExit();
+            
+            return temp;
+        }
+
+        private bool TryToPickFile(out string file)
+        {
+            var openFile = new OpenFileDialog { Filter = @"Solution|*.sln|Project|*.csproj" };
+            var dialogResult = openFile.ShowDialog();
+            file = openFile.FileName;
+            return dialogResult == DialogResult.OK;
+        }
+
+        public bool CanLoad { get; private set; }
+
+        public string Filename { get; private set; }
     }
 }
