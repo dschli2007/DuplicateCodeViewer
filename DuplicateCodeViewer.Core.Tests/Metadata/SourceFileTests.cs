670bd9618b83e8706e89c43e94819166461e506d
Dirceu <dschli2007@gmail.com>
2018-01-27 09:04:06 -0200

    Add SourceFileTests

---
+++ DuplicateCodeViewer.Core.Tests/Metadata/SourceFileTests.cs
@@ -0,0 +1,19 @@
+﻿using DuplicateCodeViewer.Core.Metadata;
+using NUnit.Framework;
+
+namespace DuplicateCodeViewer.Core.Tests.Metadata
+{
+    [TestFixture]
+   public class SourceFileTests
+    {
+        [TestCase("")]
+        [TestCase("file.txt")]
+        [TestCase(@"c:\dir\file.txt")]
+        public void ToString_WhenHasFilename_ShouldReturnFilename(string filename)
+        {
+            var obj = new SourceFile(filename);
+            Assert.AreEqual(filename, obj.ToString());
+        }
+
+    }
+}
